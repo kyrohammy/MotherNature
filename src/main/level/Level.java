0314e2b0fe4ef66eda0c66c9edfb370f02229e29
Hunter Hammond <kyrohammy@gmail.com>
2016-05-26 18:59:55 -0500

    Working on random level generation

---
+++ src/main/level/Level.java
@@ -0,0 +1,36 @@
+package main.level;
+
+import main.graphics.Screen;
+
+public class Level {
+	
+	protected int width, height;
+	protected int[] tiles;
+	
+	public Level (int width, int height) {
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		generateLevel();
+	}
+	
+	public Level (String path) {
+		loadLevel(path);
+	}
+
+	protected void generateLevel() {
+	}
+	
+	private void loadLevel (String path) {
+	}
+	
+	public void update() {
+	}
+	
+	private void time() {
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen) {	
+	}
+
+}
